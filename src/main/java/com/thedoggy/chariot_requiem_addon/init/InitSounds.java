74bbcb3631b7b2b1651c3902a5ed0a2d950cbb2f
TheDoGGySL <168200821+TheDoGGySL@users.noreply.github.com>
2024-05-05 23:38:59 +0300

    Add files via upload
    Updated sounds

--- src/main/java/com/thedoggy/chariot_requiem_addon/init/InitSounds.java
+++ src/main/java/com/thedoggy/chariot_requiem_addon/init/InitSounds.java
@@ -16,15 +16,23 @@ public class InitSounds {
             ForgeRegistries.SOUND_EVENTS, ChariotRequiem.MOD_ID); // TODO sounds.json
 
 
-    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_SUMMON_SOUND = ModSounds.STAND_SUMMON_DEFAULT;
+    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_SUMMON_SOUND = SOUNDS.register("scr_summon",
+            () -> new SoundEvent(new ResourceLocation(ChariotRequiem.MOD_ID, "scr_summon")));
+
+    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_UNSUMMON_SOUND = SOUNDS.register("scr_unsummon",
+            () -> new SoundEvent(new ResourceLocation(ChariotRequiem.MOD_ID,"scr_unsummon")));
     
-    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_UNSUMMON_SOUND = ModSounds.STAND_UNSUMMON_DEFAULT;
-    
-    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_PUNCH_LIGHT = ModSounds.STAND_PUNCH_LIGHT;
-    
-    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_PUNCH_HEAVY = ModSounds.STAND_PUNCH_HEAVY;
-    
-    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_PUNCH_BARRAGE = ModSounds.STAND_PUNCH_LIGHT;
+    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_PUNCH_LIGHT = SOUNDS.register("scr_punch",
+            () -> new SoundEvent(new ResourceLocation(ChariotRequiem.MOD_ID,"scr_punch")));
+    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_PUNCH_HEAVY = SOUNDS.register("scr_punch_heavy",
+            () -> new SoundEvent(new ResourceLocation(ChariotRequiem.MOD_ID,"scr_punch_heavy")));
+
+    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_AURA = SOUNDS.register("scr_ability_1",
+            () -> new SoundEvent(new ResourceLocation(ChariotRequiem.MOD_ID,"scr_ability_1")));
+
+    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_VIRUS = SOUNDS.register("scr_ability_2",
+            () -> new SoundEvent(new ResourceLocation(ChariotRequiem.MOD_ID,"scr_ability_2")));
+    public static final Supplier<SoundEvent> CHARIOT_REQUIEM_PUNCH_BARRAGE = InitSounds.CHARIOT_REQUIEM_PUNCH_LIGHT;
 
     public static final OstSoundList CHARIOT_REQUIEM_OST = new OstSoundList(
             new ResourceLocation(ChariotRequiem.MOD_ID, "chariot_requiem_ost"), SOUNDS);
